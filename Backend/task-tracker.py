57cbd6e879ee997ecbd56bf5ee4aae4775378050
hanif0526 <hanifabdurrahmanarifai@gmail.com>
2026-05-16 23:38:20 +0700

    update

--- Backend/task-tracker.py
+++ Backend/task-tracker.py
@@ -1,2 +1,29 @@
 #Fri, May 15
 
+
+import json, operator, sys
+
+from argparse import ArgumentParser
+from datetime import datetime
+from inspect import signature
+from pathlib import Path
+from typing import (
+	Annotated,
+	Callable,
+	Literal,
+	Optional,
+	TypeAlias,
+	TypedDict,
+	Union,
+	get_args,
+	get_origin,
+)
+	
+from tabulate import tabulate
+
+supported_queries: dict[str, dict] = {}
+TaskStatus: TypeAlias = Literal["done", "in-progress", "todo"]
+DatabaseRow = TypedDict(
+	"DatabaseRow",
+	{"description": str, "status": TaskStatus, "created-at": str, "update-at": str},
+)
